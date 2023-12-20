6bf47937edb24de089c1c2c574b6e6e927ee2170
Željka Matović <matoviczeljkaa@gmail.com>
2023-12-20 13:01:16 +0100

    Update database battery model

--- src/main/java/com/powerplant/demo/model/Battery.java
+++ src/main/java/com/powerplant/demo/model/Battery.java
@@ -1,10 +1,12 @@
 package com.powerplant.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Battery {
@@ -12,8 +14,15 @@ public class Battery {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Column(nullable = false)
 	private String name;
+	
+	@NotBlank
+	@Column(nullable = false)
 	private String postcode;
+	
+	@Column(nullable = false)
 	private int wattCapacity;
 	
 	public String getName() {
